28a66c45589932dfcf919f060eddae10e32f6729
STONE-Z00 <stonemug5@gmail>
2026-07-01 16:26:35 +0300

    commiting changes in the frontend routes

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -9,7 +9,24 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
-    <div className="bg-Slate-500">
+    <div>
+      <div className='bg-slate-100
+       h-20 text-2xl 
+       flex items-center
+        justify-center'
+        font-bold>
+       <p className='font-bold'>Stone's Vault</p> 
+        <button className="absolute
+         hover:cursor-pointer 
+         right-10
+          hover:bg-slate-500
+           bg-yellow-500
+           py-2 px-4 rounded-md
+           text-white ml-100 
+            font-normal">
+          Add button
+        </button>
+      </div>
       <Outlet />
     </div>
     </>
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -4,20 +4,13 @@ export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
   return (
-    <div>
-      <div className='bg-slate-100 h-20 text-2xl flex items-center justify-center'>
-        Stone's Vault
-        <button className="absolute hover:cursor-pointer right-10 hover:bg-slate-500 bg-slate-600 py-2 px-4 rounded-md text-white ml-100 font-normal">
-          Add button
-        </button>
-      </div>
-      <h1 className="text-4xl font-bold">You have no notes</h1>
-      <p className="mt-4 text-lg">
-        Create your first note by clicking the
-         <button className="absolute hover:cursor-pointer right-10 hover:bg-slate-500 bg-slate-600 py-2 px-4 rounded-md text-white ml-100 font-normal">
-          Add button
-        </button>
-      </p>
+    <div className="min-h-screen flex flex-col items-center justify-center text-center px-4">
+      <h1 className="text-4xl font-normal font-semibold">You have no notes</h1>
+      <button className="mt-6 hover:cursor-pointer
+       hover:bg-slate-400 bg-yellow-500 py-2 px-4 rounded-md
+        text-white font-normal">
+        Add button
+      </button>
     </div>
   )
 }
